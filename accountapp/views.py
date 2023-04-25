c588c0478b2700b75da28fe3e61eceb0752e560d
NAJIRAGI <najiragi9250@gamil.com>
2023-04-25 09:35:18 +0900

    djust django course 16 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -3,4 +3,11 @@ from django.shortcuts import render
 
 
 def helloworld(request):
-    return render(request, 'accountapp/hello_world.html')
+    #요청이 POST 방식일 경우
+    if request.method == "POST":
+                                                              #추가적인 Data 꾸러미 안에 'text'라는 이름을 가진 'POST METHOD!!'라는 내용을 넣어서 저장
+        return render(request, 'accountapp/hello_world.html', context={'text':'POST METHOD!!'})
+    #요청이 GET 방식일 경우
+    else:
+                                                              #추가적인 Data 꾸러미 안에 'text'라는 이름을 가진 'GET METHOD!!'라는 내용을 넣어서 저장
+        return render(request, 'accountapp/hello_world.html', context={'text':'GET METHOD!!'})
